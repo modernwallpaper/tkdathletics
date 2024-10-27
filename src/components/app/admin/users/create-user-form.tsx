8d144e086cb8a2bda63548889f8e85f278144e45
modernwallpaper <j.cunz@grimmels.de>
2024-10-27 15:40:53 +0100

    [~] Improved mobile experience

--- src/components/app/admin/users/create-user-form.tsx
+++ src/components/app/admin/users/create-user-form.tsx
@@ -22,6 +22,7 @@ import { Button } from "@/components/ui/button";
 import { useRegister } from "@/hooks/useRegister";
 import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const CreateUserForm = () => {
   const form = useForm<z.infer<typeof CreateUserSchema>>({
@@ -35,92 +36,94 @@ export const CreateUserForm = () => {
   };
 
   return (
-    <div className="flex flex-col gap-y-2 w-[50%]">
+    <div className="flex flex-col gap-y-2 w-[480px]">
       <CardHeader>
         <CardTitle>Create a user</CardTitle>
         <CardDescription>Enter the new user info</CardDescription>
       </CardHeader>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField
-            disabled={loading}
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input {...field} placeholder="someone" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            disabled={loading}
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input {...field} type="email" placeholder="someone" />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormField
-            disabled={loading}
-            control={form.control}
-            name="authority"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Authority</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder={"Select a role"} />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="ADMIN">Admin</SelectItem>
-                    <SelectItem value="USER">User</SelectItem>
-                  </SelectContent>
-                </Select>
-              </FormItem>
-            )}
-          />
-          <FormField
-            disabled={loading}
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <FormControl>
-                  <Input {...field} type="password" placeholder="******" />
-                </FormControl>
-                <FormDescription>Minimum of 6 characters</FormDescription>
-              </FormItem>
-            )}
-          />
-          <Separator className="mt-3" />
-          <div className="flex gap-x-2">
-            <Button
+        <ScrollArea>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full p-2">
+            <FormField
               disabled={loading}
-              asChild
-              className="w-full mt-3"
-              variant={"secondary"}
-            >
-              <a href="/v1/admin">Cancel</a>
-            </Button>
-            <Button disabled={loading} className="w-full mt-3" type="submit">
-              <p>Create</p>
-            </Button>
-          </div>
-        </form>
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="someone" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              disabled={loading}
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="email" placeholder="someone" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              disabled={loading}
+              control={form.control}
+              name="authority"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Authority</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder={"Select a role"} />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="ADMIN">Admin</SelectItem>
+                      <SelectItem value="USER">User</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormItem>
+              )}
+            />
+            <FormField
+              disabled={loading}
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="password" placeholder="******" />
+                  </FormControl>
+                  <FormDescription>Minimum of 6 characters</FormDescription>
+                </FormItem>
+              )}
+            />
+            <Separator className="mt-3" />
+            <div className="flex gap-x-2">
+              <Button
+                disabled={loading}
+                asChild
+                className="w-full mt-3"
+                variant={"secondary"}
+              >
+                <a href="/v1/admin">Cancel</a>
+              </Button>
+              <Button disabled={loading} className="w-full mt-3" type="submit">
+                <p>Create</p>
+              </Button>
+            </div>
+          </form>
+        </ScrollArea>
       </Form>
     </div>
   );
